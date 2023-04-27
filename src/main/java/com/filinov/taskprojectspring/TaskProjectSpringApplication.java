875d9504bdbe9cf4cc00b5a786569df46ff22043
Vladimir <fbvbnc@gmail.com>
2023-04-27 13:04:20 +0300

    add update method

---
+++ src/main/java/com/filinov/taskprojectspring/TaskProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.filinov.taskprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskProjectSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filinov/taskprojectspring/TaskProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filinov.taskprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProjectSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
